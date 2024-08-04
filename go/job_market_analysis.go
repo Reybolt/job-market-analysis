4087568b30088bf8d9c937b55417b2a174959211
Reybolt <reynaldoarteaga3@gmail.com>
2024-08-04 21:28:17 +1000

    Loop through request payloads and make a separate request.

--- go/job_market_analysis.go
+++ go/job_market_analysis.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
 
 type IndeedPayload struct {
 	City     string `json:"city"`
@@ -8,7 +12,33 @@ type IndeedPayload struct {
 }
 
 func getJobBreakdown(requestPayload []IndeedPayload) {
-	fmt.Println(requestPayload)
+
+	for i := 0; i < len(requestPayload); i++ {
+		fmt.Println(requestPayload[i])
+		// TODO: Make these calls concurrent.
+		indeedRequest(requestPayload[i])
+	}
+
+}
+
+func indeedRequest(IndeedPayload) {
+	// Copied from https://gobyexample.com/http-client
+	resp, err := http.Get("https://ca.indeed.com/jobs?q=software+developer&l=Ottawa%2C+ON")
+	// resp, err := http.Get("https://gobyexample.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 5; i++ {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
